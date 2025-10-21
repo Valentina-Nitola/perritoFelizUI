6b6538be0391c0d70f5284f97bcd588565015062
Valentina-Nitola <valentina.nitola@correounivalle.edu.co>
2025-10-21 18:46:13 -0500

    se agrega validacion para iniciar sesion

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import { useNavigate } from 'react-router-dom'
+
 
 const DefaultLayout = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token') // o la variable que uses para sesión
+    if (!token) {
+      navigate('/login', { replace: true }) // redirige al login si no hay token
+    }
+  }, [navigate])
   return (
     <div>
       <AppSidebar />
